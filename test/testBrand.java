6d2a5372eb2aa4e5b8abe6cedcad28514ab28b7b
Michael Cuison <xurpas7@gmail.com>
2022-10-21 16:54:09 +0800

    gk20221021

---
+++ test/testBrand.java
@@ -0,0 +1,69 @@
+
+import org.xersys.commander.base.Nautilus;
+import org.xersys.commander.base.Property;
+import org.xersys.commander.base.SQLConnection;
+import org.xersys.commander.crypt.CryptFactory;
+import org.xersys.commander.iface.LRecordMas;
+import org.xersys.parameters.base.Brand;
+
+public class testBrand {
+    public static void main(String [] args){
+        final String PRODUCTID = "Daedalus";
+        
+        //get database property
+        Property loConfig = new Property("db-config.properties", PRODUCTID);
+        if (!loConfig.loadConfig()){
+            System.err.println(loConfig.getMessage());
+            System.exit(1);
+        } else System.out.println("Database configuration was successfully loaded.");
+        
+        //connect to database
+        SQLConnection loConn = new SQLConnection();
+        loConn.setProperty(loConfig);
+        if (loConn.getConnection() == null){
+            System.err.println(loConn.getMessage());
+            System.exit(1);
+        } else
+            System.out.println("Connection was successfully initialized.");        
+        
+        //load application driver
+        Nautilus loNautilus = new Nautilus();
+        
+        loNautilus.setConnection(loConn);
+        loNautilus.setEncryption(CryptFactory.make(CryptFactory.CrypType.AESCrypt));
+        
+        loNautilus.setUserID("000100210001");
+        if (!loNautilus.load(PRODUCTID)){
+            System.err.println(loNautilus.getMessage());
+            System.exit(1);
+        } else
+            System.out.println("Application driver successfully initialized.");
+        
+        LRecordMas _listener = new LRecordMas() {
+            @Override
+            public void MasterRetreive(String fsFieldNm, Object foValue) {
+                System.out.println(fsFieldNm + "-->" + foValue);
+            }
+
+            @Override
+            public void MasterRetreive(int fnIndex, Object foValue) {
+                System.out.println(fnIndex + "-->" + foValue);
+            }
+        };
+        
+        Brand instance = new Brand(loNautilus, (String) loNautilus.getBranchConfig("sBranchCd"), false);
+        instance.setListener(_listener);
+        
+        if (instance.NewRecord()) {
+            instance.setMaster("sBrandCde", "MAC");
+            instance.setMaster("sDescript", "Michael Cuison");
+            instance.setMaster("sInvTypCd", "MC"); //sInvTypCd
+            
+            if (instance.SaveRecord()){
+                System.out.println("Record saved successfully.");
+            } else {
+                System.err.println(instance.getMessage());
+            }
+        }
+    }
+}
